cb037cc9534c2dbd0f91cbc69955a9d3c2f0b45d
Fgarcia19 <40769836+Fgarcia19@users.noreply.github.com>
2019-04-27 02:20:35 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 int main() {
     int c,x,y,m,n;
+    //donde c=cantidad de cargas, x=longitud del eje x del plano, y=logintud del eje y del plano, m=cantidad de particiones del eje x, n=cantidad de particiones del eje y;
     cin>>c;
     cin>>x;
     cin>>y;
